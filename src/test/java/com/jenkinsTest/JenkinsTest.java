69b86f77b8ab64fcd0c7d9cb6aa3432ccfded59e
Nirbhay The Omen <Nirbhay The Omen@LAPTOP-VTMNI8FB>
2022-05-31 10:04:19 +0530

    first commit new

---
+++ src/test/java/com/jenkinsTest/JenkinsTest.java
@@ -0,0 +1,14 @@
+package com.jenkinsTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	
+	@Test
+	public void demoJenkins1()
+	{
+		Reporter.log("print success",true);
+	}
+
+}
